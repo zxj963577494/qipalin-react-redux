ba960aa484cb458a682138f8a5aa4fb1d5ea95f3
Xujiang Zheng <963577494@qq.com>
2017-11-15 10:46:19 +0800

    fix

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,8 @@ const BASE_URL = config.BASE_URL
 
 // 文章
 export const POSTS = BASE_URL + '/posts'
+// 图片
+export const POSTS = BASE_URL + '/picture'
 // 类别
 export const CATEGORIES = BASE_URL + '/categories'
 // 标签
@@ -14,3 +16,17 @@ export const COMMENTS = BASE_URL + '/comments'
 export const USERS = BASE_URL + '/users'
 // 设置
 export const SETTINGS = BASE_URL + '/settings'
+// 登录
+export const SETTINGS = BASE_URL + '/token'
+// 一周热点文章
+export const SETTINGS = BASE_URL + '/hotpostthisweek'
+// 一月热点文章
+export const SETTINGS = BASE_URL + '/hotpostthismonth'
+// 全部热点文章
+export const SETTINGS = BASE_URL + '/hotpost'
+// 更新文章浏览数
+export const SETTINGS = BASE_URL + '/addpageview'
+// 点赞
+export const SETTINGS = BASE_URL + '/like'
+// 是否已点赞
+export const SETTINGS = BASE_URL + '/islike'
